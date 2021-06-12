db066a89b16ddc71ee93067303898368d336a796
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2021-06-12 13:33:19 +0900

    save file

--- SwiftUI-Section5-10/ContentView.swift
+++ SwiftUI-Section5-10/ContentView.swift
@@ -7,10 +7,74 @@
 
 import SwiftUI
 
+// P. 272
+
+extension UIApplication {
+    func endEditing() {
+        // キーボードを下げるための処理
+        sendAction(
+            #selector(UIResponder.resignFirstResponder),
+            to: nil, from: nil, for: nil
+        )
+    }
+}
+
+// P. 274
+func saveText(_ textData: String, _ fileName: String) {
+    guard let url = docURL(fileName) else {
+        return
+    }
+    
+    do {
+        let path = url.path
+        try textData.write(toFile: path, atomically:true, encoding: .utf8)
+    } catch let error as NSError {
+        print(error)
+    }
+}
+
+// P. 275
+func docURL(_ fileName: String) -> URL? {
+    let fileManager = FileManager.default
+    do {
+        let docsUrl = try fileManager.url(
+            for: .documentDirectory,
+            in: .userDomainMask,
+            appropriateFor: nil,
+            create: false)
+        let url = docsUrl.appendingPathComponent(fileName)
+        return url
+    } catch {
+        return nil
+    }
+}
+
 struct ContentView: View {
+    @State private var theText: String = """
+Hello world
+Hello world
+"""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            TextEditor(text: $theText)
+                .lineSpacing(10)
+                .border(Color.gray)
+                .padding([.leading, .bottom, .trailing])
+                .navigationTitle("メモ")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            // キーボードを下げる処理
+                            UIApplication.shared.endEditing()
+                            
+                            // saveText(theText, "sample.txt")
+                        } label : {
+                            Text("保存")
+                        }
+                    }
+                }
+        }
     }
 }
 
